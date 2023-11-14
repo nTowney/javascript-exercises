b6e84c17ef101edf5bb015c7c15bcf4695b4c15a
nTowney <144981300+nTowney@users.noreply.github.com>
2023-11-13 21:52:50 -0800

    Working my way through the js exercises
    Hope this is pushing into my repo and not trying to be a pull request...

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
+const repeatString = function(name, numOfRuns) {
+for (i=0; i <= numOfRuns; i++){
+    } if (numOfRuns < 0){
+        return "ERROR"
+    } else {
+    return name.repeat(numOfRuns)}
+}
 
 // Do not edit below this line
 module.exports = repeatString;
