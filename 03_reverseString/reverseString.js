0384d83e73229f8da2f4a601db46e24d21e887d5
nTowney <144981300+nTowney@users.noreply.github.com>
2023-11-22 22:57:23 -0800

    Completed exercises 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(reverseWord) {
+if (reverseWord.length === 0){
+    return "";
+}
+const split = reverseWord.split("");
+let wordLength = split.length-1;
+let reversed = split[wordLength]
+for (i = wordLength-1; i>=0; i--)
+    reversed += split[i];
+return reversed;
+}
 
-};
+
+
+// take the word and split each letter into an array
+// get the length of the word and use the value as the index number for
+// adding each letter last to first by decrementing the index number until 0
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,39 @@
-const removeFromArray = function() {
+const removeFromArray = function removeFromArray(array, ...itemToRemove) {
+    
+    // I'm going to try and use the rest parameter to take multiple arguments
+    // This makes the remaining arguments an array;
+    // With the arguments in an array, I should be able to use a loop to check each one until finished
+    // becomes: [[1, 2, 3, 4], 3, 2]
+    // 1,2,3,43,2
 
-};
+    // the for loop should loop through for..of
 
+    const args = itemToRemove;
+    // this turns all arguments into an array
+
+    // console.log(args);
+
+    for (const arg of args){
+        // console.log(arg);
+        // going to check if the item is in index 0
+        if (array.indexOf(arg) === 0){
+            array.splice(0, 1);
+        } else if (array.indexOf(arg) > 0){
+            // this checks to see if the item is in the array
+            // console.log(array.indexOf(arg));
+            // console.log(arg);
+            array.splice(array.indexOf(arg), 1);
+        }
+        // console.log(array);
+    }
+return array;
+//    if (array.indexOf(itemToRemove) > 0){
+//     array.splice(array.indexOf(itemToRemove), 1);
+//    }
+}
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// this bit of code works to remove at least one item
+// array.splice(array.indexOf(...itemToRemove), 1)
